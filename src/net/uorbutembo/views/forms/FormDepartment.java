af11900925608e5ab365df2befbd997b7b06149c
Esaie MUHASA <esaiemuhasa.dev@gmail.com>
2022-03-20 18:07:04 +0200

    Retouche des quelques formutaires, et supports des requette de modifications

--- src/net/uorbutembo/views/forms/FormDepartment.java
+++ src/net/uorbutembo/views/forms/FormDepartment.java
@@ -13,6 +13,7 @@ import javax.swing.JOptionPane;
 
 import net.uorbutembo.beans.Department;
 import net.uorbutembo.beans.Faculty;
+import net.uorbutembo.dao.DAOAdapter;
 import net.uorbutembo.dao.DAOException;
 import net.uorbutembo.dao.DepartmentDao;
 import net.uorbutembo.dao.FacultyDao;
@@ -30,25 +31,54 @@ public class FormDepartment extends DefaultFormPanel {
 	
 	private final FormGroup<String> acronym = FormGroup.createTextField("Abbreviation");
 	private final FormGroup<String> fullname = FormGroup.createTextField("Appelation complete");
-	private final FormGroup<Faculty> faculty = FormGroup.createComboBox("Faculté");
+	
+	private final ComboBox<Faculty> comboFaculties = new ComboBox<>("Faculté");
+	private final FormGroup<Faculty> faculty = FormGroup.createComboBox(comboFaculties);
 	
 	private FacultyDao facultyDao;
 	private DepartmentDao departmentDao;
+	private Department department;//lors de la modification, cette reference est != null
 	
 	public FormDepartment(MainWindow mainWindow, DepartmentDao departmentDao) {
 		super(mainWindow);
 		this.departmentDao = departmentDao;
 		this.facultyDao = departmentDao.getFactory().findDao(FacultyDao.class);
-		this.setTitle("Formulaire d'enregistrement");
 		this.init();
+		
+		facultyDao.addListener(new DAOAdapter<Faculty>() {
+			@Override
+			public void onCreate(Faculty e, int requestId) {
+				comboFaculties.addItem(e);
+			}
+			
+			@Override
+			public void onUpdate(Faculty e, int requestId) {
+				for (int i = 0, max = comboFaculties.getItemCount(); i<max; i++) {
+					if(comboFaculties.getItemAt(i).getId() == e.getId()) {
+						comboFaculties.removeItemAt(i);
+						comboFaculties.addItem(e);
+						return;
+					}
+				}
+			}
+			
+			@Override
+			public void onDelete(Faculty e, int requestId) {
+				for (int i = 0, max = comboFaculties.getItemCount(); i<max; i++) {
+					if(comboFaculties.getItemAt(i).getId() == e.getId()) {
+						comboFaculties.removeItemAt(i);
+						return;
+					}
+				}
+			}
+		});
 	}
 	
 	private void init() {
 		
 		List<Faculty> facs = this.facultyDao.findAll();
-		ComboBox<Faculty> compo = (ComboBox<Faculty>) this.faculty.getField();
 		for (Faculty fac : facs) {
-			compo.addItem(fac);
+			comboFaculties.addItem(fac);
 		}
 		
 		final Box box = Box.createVerticalBox();
@@ -60,19 +90,52 @@ public class FormDepartment extends DefaultFormPanel {
 		this.getBody().add(box, BorderLayout.CENTER);
 	}
 	
+	/**
+	 * @param department the department to set
+	 */
+	public void setDepartment(Department department) {
+		this.department = department;
+		if (department  != null) {
+			acronym.getField().setValue(department.getAcronym());
+			fullname.getField().setValue(department.getName());
+
+			for (int i = 0, max = comboFaculties.getItemCount(); i<max; i++) {
+				if(comboFaculties.getItemAt(i).getId() == department.getFaculty().getId()) {
+					comboFaculties.setSelectedIndex(i);
+					break;
+				}
+			}
+			setTitle(TITLE_2);
+		} else {
+			acronym.getField().setValue("");
+			fullname.getField().setValue("");
+			setTitle(TITLE_1);
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		String acronym = this.acronym.getValue();
 		String name = this.fullname.getValue();
+		
+		Date now =  new Date();
 		Faculty fac = this.faculty.getValue();
 		Department dep = new Department();
 		dep.setFaculty(fac);
 		dep.setName(name);
 		dep.setAcronym(acronym);
-		dep.setRecordDate(new Date());
+		
 		try {
-			this.departmentDao.create(dep);
+			if(department == null) {
+				dep.setRecordDate(now);			
+				this.departmentDao.create(dep);
+			} else {
+				dep.setLastUpdate(now);
+				departmentDao.update(dep, department.getId());
+			}
+			
 			this.showMessageDialog("Info", name+"\nEnregistrer avec succes", JOptionPane.INFORMATION_MESSAGE);
+			setDepartment(null);
 		} catch (DAOException e) {
 			this.showMessageDialog("Erreur", e.getMessage(), JOptionPane.ERROR_MESSAGE);
 		}
